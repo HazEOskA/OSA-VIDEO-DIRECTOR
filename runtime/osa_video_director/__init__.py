0d05f6403cfeca9a6f7e3c400719ffb1ff5accc4
Bartosz OSA <133907273+HazEOskA@users.noreply.github.com>
2026-09-21 19:28:27 +0200

    feat: export execution router

--- runtime/osa_video_director/__init__.py
+++ runtime/osa_video_director/__init__.py
@@ -1,10 +1,11 @@
 from .director import DirectorRuntime
 from .models import DirectorDNA, EditAction, EditPlan, MediaAsset, PostProductionJob, VerificationIssue, VerificationReport
 from .repair import RepairPlanner
+from .router import ExecutionRouter, RoutingError
 from .verifier import TimelineVerifier
 
 __all__ = [
     "DirectorRuntime", "DirectorDNA", "EditAction", "EditPlan", "MediaAsset",
     "PostProductionJob", "VerificationIssue", "VerificationReport",
-    "RepairPlanner", "TimelineVerifier",
+    "RepairPlanner", "ExecutionRouter", "RoutingError", "TimelineVerifier",
 ]
