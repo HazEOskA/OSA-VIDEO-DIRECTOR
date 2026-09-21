3ad2d6ccb5cf1780549016a70d76b76fb6979a0b
Bartosz OSA <133907273+HazEOskA@users.noreply.github.com>
2026-09-21 19:28:25 +0200

    feat: export FFmpeg adapter

--- runtime/osa_video_director/adapters/__init__.py
+++ runtime/osa_video_director/adapters/__init__.py
@@ -1,6 +1,14 @@
 from .base import AdapterCommand, PostProductionAdapter
 from .davinci import DaVinciResolveAdapter
+from .ffmpeg import FFmpegAdapter
 from .higgsfield import HiggsfieldAdapter
 from .remotion import RemotionAdapter
 
-__all__ = ["AdapterCommand", "PostProductionAdapter", "DaVinciResolveAdapter", "HiggsfieldAdapter", "RemotionAdapter"]
+__all__ = [
+    "AdapterCommand",
+    "PostProductionAdapter",
+    "DaVinciResolveAdapter",
+    "FFmpegAdapter",
+    "HiggsfieldAdapter",
+    "RemotionAdapter",
+]
